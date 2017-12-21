bd1b4a1bf4149912e0e116b56e146ed142168620
Code Annihilator <frostberryart@gmail.com>
2017-12-21 15:51:28 +0300

    project foundation

---
+++ seed/product-seeder.js
@@ -0,0 +1,45 @@
+var Product = require('../models/product');
+
+var mongoose = require('mongoose');
+mongoose.connect('localhost:27017/shopping');
+
+var products = [
+  new Product({
+    imagePath: 'https://www.franchiseindia.com/uploads/content/wi/art/organic-foods-are-healthier-e40169e174.jpg',
+    title: 'Some Food',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    price: 10
+  }),
+  new Product({
+    imagePath: 'https://www.franchiseindia.com/uploads/content/wi/art/organic-foods-are-healthier-e40169e174.jpg',
+    title: 'Some Food',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    price: 10
+  }),
+  new Product({
+    imagePath: 'https://www.franchiseindia.com/uploads/content/wi/art/organic-foods-are-healthier-e40169e174.jpg',
+    title: 'Some Food',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    price: 10
+  }),
+  new Product({
+    imagePath: 'https://www.franchiseindia.com/uploads/content/wi/art/organic-foods-are-healthier-e40169e174.jpg',
+    title: 'Some Food',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    price: 10
+  })
+];
+
+var done = 0;
+for (var i = 0; i < products.length; i++) {
+  products[i].save(function(err, result) {
+    done++;
+    if (done === products.length) {
+      exit();
+    }
+  });
+}
+
+function exit() {
+  mongoose.disconnect();
+}
