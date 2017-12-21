fb57e71db0ff870caac7f7189957f643d624be07
Code Annihilator <frostberryart@gmail.com>
2017-12-21 18:04:18 +0300

    signup validation

--- config/passport.js
+++ config/passport.js
@@ -18,6 +18,16 @@ passport.use('local.signup', new LocalStrategy({
   passwordField: 'password',
   passReqToCallback: true
 }, function(req, email, password, done) {
+  req.checkBody('email', 'Invalid email').notEmpty().isEmail();
+  req.checkBody('password', 'Invalid password').notEmpty().isLength({ min: 4 });
+  var errors = req.validationErrors();
+  if (errors) {
+    var messages = [];
+    errors.forEach(function(error) {
+      messages.push(error.msg);
+    });
+    return done(null, false, req.flash('error', messages));
+  }
   User.findOne({ 'email': email }, function(err, user) {
     if (err) return done(err);
     if (user) return done(null, false, { message: 'Email is already in use' });
@@ -30,3 +40,11 @@ passport.use('local.signup', new LocalStrategy({
     });
   });
 }));
+
+// passport.use('local.signin', new LocalStrategy({
+//   usernameField: 'email',
+//   passwordField: 'password',
+//   passReqToCallback: true
+// }, function(req, email, password, done) {
+//
+// }));
