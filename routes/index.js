92c0c4cc1fd52346a3ecdf98e61a1fe19cb7af05
Code Annihilator <frostberryart@gmail.com>
2017-12-28 21:04:57 +0300

    stripe payments added

--- routes/index.js
+++ routes/index.js
@@ -5,13 +5,14 @@ var Cart = require('../models/cart');
 var Product = require('../models/product');
 
 router.get('/', function(req, res, next) {
+  var successMsg = req.flash('success')[0];
   Product.find(function(err, docs) {
     var productChunks = [];
     var chunkSize = 3;
     for (var i = 0; i < docs.length; i += chunkSize) {
       productChunks.push(docs.slice(i, i + chunkSize))
     }
-    res.render('shop/index', { title: 'Express', products: productChunks });
+    res.render('shop/index', { title: 'Express', products: productChunks, successMsg: successMsg, noMessages: !successMsg });
   });
 });
 
@@ -31,6 +32,35 @@ router.get('/shopping-cart', function(req, res, next) {
   if (!req.session.cart) return res.render('shop/shopping-cart', { products: null });
   var cart = new Cart(req.session.cart);
   res.render('shop/shopping-cart', { products: cart.generateArray(), totalPrice: cart.totalPrice });
+});
+
+router.get('/checkout', function(req, res, next) {
+  if (!req.session.cart) return res.redirect('/shopping-cart');
+  var cart = new Cart(req.session.cart);
+  var errMsg = req.flash('error')[0];
+  res.render('shop/checkout', { total: cart.totalPrice, errMsg: errMsg, noError: !errMsg });
+});
+
+router.post('/checkout', function(req, res, next) {
+  if (!req.session.cart) return res.redirect('/shopping-cart');
+  var cart = new Cart(req.session.cart);
+  var stripe = require("stripe")("sk_test_YVNd7kHmQsZv3jrQYNlQPIQV");
+  console.log(req.body);
+
+  stripe.charges.create({
+    amount: cart.totalPrice * 100,
+    currency: "usd",
+    source: req.body.stripeToken,
+    description: "Test Charge"
+  }, function(err, charge) {
+    if (err) {
+      req.flash('error', err.message);
+      return res.redirect('/checkout');
+    }
+    req.flash('success', 'Successfully bought product!');
+    req.session.cart = null;
+    res.redirect('/');
+  });
 });
 
 router.get('*', function(req, res, next) {
