7a56484100096b08c3f49d98b8118926393d7b11
Code Annihilator <frostberryart@gmail.com>
2017-12-30 20:05:11 +0300

    order status done, cart checkout with auth done

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var Cart = require('../models/cart');
 var Product = require('../models/product');
+var Order = require('../models/order');
 
 router.get('/', function(req, res, next) {
   var successMsg = req.flash('success')[0];
@@ -28,25 +29,40 @@ router.get('/add-to-cart/:id', function(req, res, next) {
   });
 });
 
+router.get('/reduce/:id', function(req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
+  cart.reduceByOne(productId);
+  req.session.cart = cart;
+  res.redirect('/shopping-cart');
+});
+
+router.get('/remove/:id', function(req, res, next) {
+  var productId = req.params.id;
+  var cart = new Cart(req.session.cart ? req.session.cart : {});
+  cart.removeItem(productId);
+  req.session.cart = cart;
+  res.redirect('/shopping-cart');
+});
+
 router.get('/shopping-cart', function(req, res, next) {
   if (!req.session.cart) return res.render('shop/shopping-cart', { products: null });
   var cart = new Cart(req.session.cart);
+  console.log(cart.generateArray());
   res.render('shop/shopping-cart', { products: cart.generateArray(), totalPrice: cart.totalPrice });
 });
 
-router.get('/checkout', function(req, res, next) {
+router.get('/checkout', isLoggedIn, function(req, res, next) {
   if (!req.session.cart) return res.redirect('/shopping-cart');
   var cart = new Cart(req.session.cart);
   var errMsg = req.flash('error')[0];
   res.render('shop/checkout', { total: cart.totalPrice, errMsg: errMsg, noError: !errMsg });
 });
 
-router.post('/checkout', function(req, res, next) {
+router.post('/checkout', isLoggedIn, function(req, res, next) {
   if (!req.session.cart) return res.redirect('/shopping-cart');
   var cart = new Cart(req.session.cart);
   var stripe = require("stripe")("sk_test_YVNd7kHmQsZv3jrQYNlQPIQV");
-  console.log(req.body);
-
   stripe.charges.create({
     amount: cart.totalPrice * 100,
     currency: "usd",
@@ -57,14 +73,29 @@ router.post('/checkout', function(req, res, next) {
       req.flash('error', err.message);
       return res.redirect('/checkout');
     }
-    req.flash('success', 'Successfully bought product!');
-    req.session.cart = null;
-    res.redirect('/');
+    var order = new Order({
+      user: req.user,
+      cart: cart,
+      address: req.body.address,
+      name: req.body.name,
+      paymentId: charge.id
+    });
+    order.save(function(err, result) {
+      req.flash('success', 'Successfully bought product!');
+      req.session.cart = null;
+      res.redirect('/');
+    });
   });
 });
 
-router.get('*', function(req, res, next) {
-  res.redirect('/');
-})
+// router.get('*', function(req, res, next) {
+//   res.redirect('/');
+// })
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect('/user/signin');
+  req.session.oldUrl = req.url;
+}
 
 module.exports = router;
